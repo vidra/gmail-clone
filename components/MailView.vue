1a69f4c757b513b411befc7184ad18ff005fe20b
TK <ottertut@gmail.com>
2022-06-05 12:34:14 +0300

    add data with axios

---
+++ components/MailView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="email-display">
+    <h2 class="mb-0">Subject: <strong>{{email.subject}}</strong></h2>
+    <div><em>From {{email.from}} on {{email.sentAt}}</em></div>
+    <div> {{email.body}} </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      email: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
